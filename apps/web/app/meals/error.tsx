6fe91be22f3ef599b272350c12cda4dc2086adb0
Ramsudharsan Manoharan <ramsudharsan.nitdgp@gmail.com>
2025-03-07 16:04:30 +0530

    Added error page

---
+++ apps/web/app/meals/error.tsx
@@ -0,0 +1,9 @@
+"use client";
+export default function MealsError() {
+  return (
+    <main className="error">
+      <h1>An error occurred!</h1>
+      <p>Failed to fetch meals please try again later.</p>
+    </main>
+  );
+}
