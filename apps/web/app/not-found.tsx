768ffd2f73622e1ef885abeb1418ef9b35d1541c
Ramsudharsan Manoharan <ramsudharsan.nitdgp@gmail.com>
2025-03-07 16:09:25 +0530

    Added not found page

---
+++ apps/web/app/not-found.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function NotFound() {
+    return (
+        <main className="not-found">
+            <h1>Not Found</h1>
+            <p>Unfortunately we couldn't find the page you are looking for.</p>
+        </main>
+    )
+}
