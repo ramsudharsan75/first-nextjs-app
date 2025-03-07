7bf4e0e56795a96d8848d2cac12d51cbe643251b
Ramsudharsan Manoharan <ramsudharsan.nitdgp@gmail.com>
2025-03-07 16:05:09 +0530

    Removed artifical error

--- apps/web/lib/meals.ts
+++ apps/web/lib/meals.ts
@@ -5,6 +5,5 @@ const db = sql("meals.db");
 
 export async function getMeals(): Promise<Meal[]> {
   await new Promise((resolve) => setTimeout(resolve, 3000));
-  throw new Error("Failed to fetch meals");
   return db.prepare("SELECT * FROM meals").all() as Meal[];
 }
